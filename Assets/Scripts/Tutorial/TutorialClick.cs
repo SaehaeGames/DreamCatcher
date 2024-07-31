ba61095c448a46b8378ed2256969089ece37c7f2
iiblueblue <iiblueblue@naver.com>
2024-07-31 17:47:07 +0900

    클릭 튜토리얼 수정중

--- Assets/Scripts/Tutorial/TutorialClick.cs
+++ Assets/Scripts/Tutorial/TutorialClick.cs
@@ -14,8 +14,15 @@ public class TutorialClick : TutorialBase
 
     private bool isDragObj;
     private ScriptBox scriptBox;
+
+    private Transform startParent;
+
+    private GameObject duplicatedClickBtn;
+
     [Header("화살표 강조 ON/OFF")]
     [SerializeField] private bool highlightArrowOnOff;
+    [SerializeField] private bool doClickButnDuplicate;
+    [SerializeField] private Sprite arrowImg;
 
     [Header("클릭/드래그 대상-입력")]
     [SerializeField] private GameObject clickBtn; // 클릭/드래그 대상
@@ -51,57 +58,37 @@ public class TutorialClick : TutorialBase
         _bottomBar = GameObject.FindGameObjectWithTag("BottomBar").GetComponent<BottomBar>();
         _gameSceneManager =GameSceneManager.Instance;
 
-        // 화살표 생성
-        
+        if(highlightArrowOnOff)
+        {
+            if(doClickButnDuplicate)
+            {
+                duplicatedClickBtn = Instantiate(clickBtn, clickBtn.transform.position, clickBtn.transform.rotation);
+            }
+            // 화살표 생성
+            arrowPrefab = Resources.Load<GameObject>("Prefabs/Tutorial/HighlightArrowPref");
+            arrow = Instantiate(arrowPrefab, new Vector2(0f, 0f), Quaternion.Euler(new Vector3(0f, 0f, 0f)));
+            arrow.GetComponent<Canvas>().worldCamera = Camera.main;
+            GameObject arrowChild = arrow.transform.GetChild(0).gameObject;
 
-        // 화살표 생성
-        arrowPrefab = Resources.Load<GameObject>("Prefabs/Tutorial/HighlightArrowPref");
-        arrow = Instantiate(arrowPrefab, new Vector2(0f, 0f), Quaternion.Euler(new Vector3(0f, 0f, 0f)));
-        arrow.GetComponent<Canvas>().worldCamera = Camera.main;
-        GameObject arrowChild = arrow.transform.GetChild(0).gameObject;
+            // 화살표 위치 조정
+            arrowChild.GetComponent<Image>().sprite = arrowImg;
 
+            // 클릭/드래그 대상 높이 조정
+            if(doClickButnDuplicate)
+            {
+                duplicatedClickBtn.transform.SetParent(arrow.transform);
+                duplicatedClickBtn.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            else
+            {
+                startParent = clickBtn.transform.parent;
+                clickBtn.transform.SetParent(arrow.transform);
+            }
 
-
-        // 화살표 위치 조정
-        RectTransform rectTransform = clickBtn.GetComponent<RectTransform>(); // 클릭 대상 위치
-        if (rectTransform == null) return;
-        Vector2 objectSize = rectTransform.sizeDelta;  // 버튼 사이즈
-        Vector3[] objectCorners = new Vector3[4];
-        rectTransform.GetWorldCorners(objectCorners);
-        Vector3 topCenter = (objectCorners[1] + objectCorners[2]) / 2;
-        Vector2 arrowSize = arrowChild.GetComponent<RectTransform>().sizeDelta;
-
-
-        //Vector2 objectPosition = clickBtn.transform.position; //버튼 위치
-
-        // 수정 전
-        /*arrowPos = new Vector2(objectPosition.x, objectPosition.y + +0.3f);
-        arrowChild.transform.position = arrowPos;
-        arrowChild.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));*/
-
-        // 수정 중
-        /*RectTransform uiCanvasRectTransform = clickBtn.transform.root.GetComponent<RectTransform>();
-        if(uiCanvasRectTransform == null) return;*/
-
-        arrowPos = new Vector3(rectTransform.position.x, topCenter.y + arrowSize.y / 2, topCenter.z);
-        Debug.Log("topCenter" + topCenter + "arrowSize" + arrowSize+"clickBtnPositionX"+rectTransform.position.x);
-
-        //Debug.Log("objectSize : " + objectSize.y / 2 + " | arrowSize : " + arrowChild.GetComponent<RectTransform>().sizeDelta.y / 2 + " | arrowPos : " + objectPosition.y + ((objectSize.y / 2) + (arrowChild.GetComponent<RectTransform>().sizeDelta.y / 2)) + " | objPosition x : " + objectPosition.x + " | objPosition y : " + objectPosition.y);
-
-        //arrowChild.transform.position = arrowPos;
-        //arrowChild.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
-
-        Vector2 canvasPos;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)rectTransform.parent, arrowPos, Camera.main, out canvasPos);
-        arrowChild.GetComponent<RectTransform>().anchoredPosition = canvasPos;
-
-        Debug.Log("afterArrowPos" + arrowChild.transform.position+"afterArrowPosLocal"+arrowChild.transform.localPosition);
-
-        arrowChild.GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0f, 0f, 90f));
-
-        // 화살표 깜박임 애니메이션 재생
-        arrowChild.GetComponent<Animator>().enabled = true;
-        arrowChild.GetComponent<Animator>().Play("blinkArrow");
+            // 화살표 깜박임 애니메이션 재생
+            arrowChild.GetComponent<Animator>().enabled = true;
+            arrowChild.GetComponent<Animator>().Play("blinkArrow");
+        }     
 
         // 패널 이동시
         if (panelChange)
@@ -112,22 +99,34 @@ public class TutorialClick : TutorialBase
             shadowPanal.GetComponent<Image>().sprite = shadowImages[panelChangeNum]; // 패널 종류 선택
             // 버튼 설정
             _bottomBar.onClickRemove(panelChangeNum);
-            clickBtn.gameObject.transform.SetParent(canvas.transform, true); // 버튼 가장 위로
-            clickBtn.gameObject.transform.SetAsLastSibling(); // 버튼 가장 위로
         }
     }
 
     public override void Execute(TutorialController controller)
     {
         // 해당 버튼을 누르면
-        if (clickBtn.GetComponent<TutorialButton>() != null && clickBtn.GetComponent<TutorialButton>().buttonClicked)
+        if (clickBtn.GetComponent<TutorialButton>() != null)
         {
-            controller.SetNextTutorial(sceneStates[panelChangeNum]); // 다음 튜토리얼
+            if (doClickButnDuplicate && duplicatedClickBtn.GetComponent<TutorialButton>().buttonClicked)
+            {
+                Destroy(duplicatedClickBtn);
+                controller.SetNextTutorial(sceneStates[panelChangeNum]); // 다음 튜토리얼
+            }
+            else if (clickBtn.GetComponent<TutorialButton>().buttonClicked)
+            {
+                clickBtn.transform.SetParent(startParent);
+                controller.SetNextTutorial(sceneStates[panelChangeNum]); // 다음 튜토리얼
+            }
         } // 해당 오브젝트를 드래그하면
-        else if(clickBtn.GetComponent<TutorialDrag>()!=null&&clickBtn.GetComponent<TutorialDrag>().GetObjectDraged())
+        else if (clickBtn.GetComponent<TutorialDrag>() != null)
         {
-            isDragObj = true;
-            controller.SetNextTutorial(SceneState.None); // 다음 튜토리얼
+            if(clickBtn.GetComponent<TutorialDrag>().GetObjectDraged())
+            {
+                Debug.Log("드래그 튜토리얼 완료");
+                clickBtn.transform.SetParent(startParent);
+                isDragObj = true;
+                controller.SetNextTutorial(SceneState.None); // 다음 토리얼
+            }
         }
     }
 
