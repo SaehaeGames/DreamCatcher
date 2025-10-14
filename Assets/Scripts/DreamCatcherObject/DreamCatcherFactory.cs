10ae5acc0ccbadaaaf19d68bdf4d7ff228bc2d74
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-10-15 04:58:35 +0900

    ✨ update : QuestDreamCatcherInfo_Object를 DreamCatcher로 변환하는 DreamCatcherFactory 추가 ✨ update : 드림캐쳐 두 개를 비교하는 함수 추가 및 테스트

---
+++ Assets/Scripts/DreamCatcherObject/DreamCatcherFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DreamCatcherFactory
+{
+    // QuestDreamCatcherInfo_Object -> DreamCatcher 변환
+    public static DreamCatcher ConvertToDreamCatcher(QuestDreamCatcherInfo_Object questDreamCatcherObject)
+    {
+        // null 체크
+        if(questDreamCatcherObject == null)
+        {
+            Debug.LogError("QuestDreamCatcherInfo_Object is null 변환 불가");
+            return null;
+        }
+
+        // 혹시 Parse가 안된 상태라면 Parse 실행
+        if(questDreamCatcherObject.lines == null || questDreamCatcherObject.beads==null)
+        {
+            questDreamCatcherObject.Parse();
+        }
+
+        return new DreamCatcher(
+            questDreamCatcherObject.id, 
+            questDreamCatcherObject.lines, 
+            questDreamCatcherObject.beads, 
+            questDreamCatcherObject.color, 
+            questDreamCatcherObject.feather1, 
+            questDreamCatcherObject.feather2, 
+            questDreamCatcherObject.feather3);
+    }
+}
--- Assets/Scripts/Quest/Delivery/QuestDeliveryChecker.cs
+++ Assets/Scripts/Quest/Delivery/QuestDeliveryChecker.cs
@@ -1,29 +1,85 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class QuestDeliveryChecker : MonoBehaviour
 {
+    List<QuestDreamCatcherInfo_Object> questDreamCatcehrInfoDatalist;
+    List<DreamCatcher> dreamCatcherListData;
     // Start is called before the first frame update
     void Start()
     {
-        // µ¥ÀÌÅÍ ºÒ·¯¿À±â
+        // ë°ì´í„° ë¶ˆëŸ¬ì˜¤ê¸°
+        questDreamCatcehrInfoDatalist = GameManager.instance.questDreamCatcherInfo_data.dataList;
+        Debug.Log("questDreamCatcherInfoDatalist : " + questDreamCatcehrInfoDatalist[0].lines[4]);
 
+        dreamCatcherListData = GameManager.instance.dreamCatcherDataManager.dreamCatcherList;
+        TryDeliveryDreamCatcher();
     }
 
-    bool IsSameDreamCatcher(DreamCatcher dreamCatcher1, DreamCatcher dreamCatcher2)
+    public void TryDeliveryDreamCatcher()
     {
-        // µå¸²Ä³ÃÄ »ö ºñ±³
-        // µå¸²Ä³ÃÄ ¸ğ¾ç ºñ±³
-        // µå¸²Ä³ÃÄ º¸¼® À§Ä¡ ºñ±³
-        // µå¸²Ä³ÃÄ ±êÅĞ ºñ±³
+        DreamCatcher questDreamCatcher = DreamCatcherFactory.ConvertToDreamCatcher(questDreamCatcehrInfoDatalist[0]);
+        bool testResult=IsSameDreamCatcher(questDreamCatcher, dreamCatcherListData[0]);
+        if(testResult)
+        {
+            Debug.Log("dreamCatcher is Same");
+        }
+        else
+        {
+            Debug.Log("dreamCatcher is not Same");
+        }
+    }
+
+    private bool IsSameDreamCatcher(DreamCatcher dreamCatcher1, DreamCatcher dreamCatcher2)
+    {
+        if(dreamCatcher1 == null || dreamCatcher2 == null)
+        {
+            Debug.LogError("dreamCatcher1 or dreamCatcher2 is null ë¹„êµ ë¶ˆê°€");
+            return false;
+        }
+
+        Debug.Log(dreamCatcher1.DCid + " vs " + dreamCatcher2.DCid);
+
+        // ë“œë¦¼ìºì³ ìƒ‰ ë¹„êµ
+        if (dreamCatcher1.DCcolor != dreamCatcher2.DCcolor)
+        {
+            Debug.Log("dreamCatcher is not Same - color");
+            return false;
+        }
+
+        // ë“œë¦¼ìºì³ ê¹ƒí„¸ ë¹„êµ
+        if(dreamCatcher1.DCfeather1 != dreamCatcher2.DCfeather1)
+        {
+            Debug.Log("dreamCatcher is not Same - feather1");
+            return false;
+        }
+        if(dreamCatcher1.DCfeather2 != dreamCatcher2.DCfeather2)
+        {
+            Debug.Log("dreamCatcher is not Same - feather2");
+            return false;
+        }
+        if(dreamCatcher1.DCfeather3 != dreamCatcher2.DCfeather3)
+        {
+            Debug.Log("dreamCatcher is not Same - feather3");
+            return false;
+        }
+
+        // ë“œë¦¼ìºì³ ëª¨ì–‘ ë¹„êµ
+        if (!dreamCatcher1.DCline.SequenceEqual(dreamCatcher2.DCline))
+        {
+            Debug.Log("dreamCatcher is not Same - lines");
+            return false;
+        }
+
+        // ë“œë¦¼ìºì³ ë³´ì„ ìœ„ì¹˜ ë¹„êµ
+        if (!dreamCatcher1.DCbead.SequenceEqual(dreamCatcher2.DCbead))
+        {
+            Debug.Log("dreamCatcher is not Same - beads");
+            return false;
+        }
 
         return true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
