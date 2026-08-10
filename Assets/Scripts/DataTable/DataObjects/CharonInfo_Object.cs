0dfd0988f9c88f2284746f8e5522dfeb22e7b7cb
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2026-08-10 20:56:36 +0900

    🎉 add: CharonInfo 데이터 추가를 위한 코드 추가

---
+++ Assets/Scripts/DataTable/DataObjects/CharonInfo_Object.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharonInfo_Object
+{
+    public string id;
+    public string title;
+    public string contents;
+    public string from;
+    public string item;
+    public string note;
+
+    public CharonInfo_Object()
+    {
+        this.id = "SO_0000";
+        this.title = this.note = this.item = this.contents = this.from = "";
+    }
+
+    public CharonInfo_Object(string _id, string _title, string _contents, string _from, string _item, string _note)
+    {
+        this.id = _id;
+        this.title = _title;
+        this.contents = _contents;
+        this.from = _from;
+        this.item = _item;
+        this.note = _note;
+    }
+
+    public string GetId()
+    {
+        return id;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public string GetContetns()
+    {
+        return contents;
+    }
+
+    public string GetFrom()
+    {
+        return from;
+    }
+
+    public string GetItem()
+    {
+        return item;
+    }
+
+    public string GetNote()
+    {
+        return note;
+    }
+}
